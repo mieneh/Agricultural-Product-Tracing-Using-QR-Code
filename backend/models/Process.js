981879bad9a5597b19e2509bbb223c7c18bb6881
mieneh <dpm210103@gmail.com>
2024-12-12 13:17:19 +0700

    feat: add core models including category, product, process, region, harvest

---
+++ backend/models/Process.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const processSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  steps: [
+    {
+      name: { type: String, required: true },
+      content: { type: String, required: true },
+    }
+  ],
+  userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Process', processSchema);
---
+++ backend/models/Product.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+  name: { type: String, required: true },
+  description: { type: String },
+  image: { type: String },
+  userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', productSchema);
