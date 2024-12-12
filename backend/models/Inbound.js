ccdef1d3d438759e9c1f5e1b4c430629e0ee4abe
mieneh <dpm210103@gmail.com>
2024-12-12 16:07:11 +0700

    feat: add core models including inbound, outbound, retailer

---
+++ backend/models/Inbound.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const inboundSchema = new mongoose.Schema({
+  batchID: { type: mongoose.Schema.Types.ObjectId, ref: 'Harvest', required: true },
+  entryDate: { type: Date, required: true },
+  storageCondition: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  remainingQuantity: { type: Number, required: true },
+  status: { type: String, default: 'Pending'},
+  userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Inbound', inboundSchema);
